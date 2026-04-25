99afef77e774a944743ad19f116c4becf71f2374
Mudassir-13 <mbaksh@hawk.iit.edu>
2026-04-25 13:46:50 -0500

    added click handler to fetch and display sunrise/sunset data

---
+++ site.js
@@ -0,0 +1,43 @@
+const locationSelect = document.querySelector('#location');
+const getDataBtn = document.querySelector('#get-data');
+const errorDiv = document.querySelector('#error-message');
+
+getDataBtn.addEventListener('click', () => {
+    errorDiv.textContent = '';
+
+    const coords = locationSelect.value.split(',');
+    const lat = coords[0];
+    const lng = coords[1];
+
+    fetchSunData(lat, lng, 'today');
+    fetchSunData(lat, 'tomorrow');
+});
+
+function fetchSunData(lat, lng, day) {
+    const url = `https://api.sunrisesunset.io/json?lat=${lat}&lng=${lng}&date=${day}`;
+
+    fetch(url)
+    .then(response => {
+        if (!respone.ok) throw new Error('Network response was not ok');
+            return response.json();
+        })
+        .then(data => {
+            updateUI(data.results, day);
+        })
+        .catch(error => {
+            errorDiv.textContent = "Error: Could not retrieve data."; 
+            console.error(error);
+        });
+}
+
+function updateUI(results, day) {
+    const prefix = (day === 'today') ? 'today' : 'tmrw';
+
+    document.getElementById(`${prefix}-sunrise`).textContent = results.sunrise;
+    document.getElementById(`${prefix}-sunset`).textContent = results.sunset;
+    document.getElementById(`${prefix}-dawn`).textContent = results.dawn;
+    document.getElementById(`${prefix}-dusk`).textContent = results.dusk;
+    document.getElementById(`${prefix}-solar-noon`).textContent = results.solar-noon;
+    document.getElementById(`${prefix}-day-length`).textContent = results.day-length;
+    document.getElementById(`${prefix}-time-zone`).textContent = results.time-zone;
+}
